4420a4b4e25826f5f2ae143cf2a68389dedbc599
pogmog-python <pogmogi@gmail.com>
2025-01-04 12:57:32 +0300

    add widget

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,4 @@
-from masks import get_mask_card_number, get_mask_account
+from masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(user_info: str) -> str:
@@ -9,6 +9,7 @@ def mask_account_card(user_info: str) -> str:
         card_num = get_mask_card_number(user_info[-16:])
         card_mask = user_info.replace(user_info[-16:], card_num)
         return card_mask
+
 
 def get_date(date: str) -> str:
     """ Функция возвращает строку с датой в формате ДД.ММ.ГГГГ"""
