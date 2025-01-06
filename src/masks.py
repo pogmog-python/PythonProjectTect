ff36f92dafd7470c5f942d21fb7fbeb59247be56
pogmog-python <pogmogi@gmail.com>
2025-01-06 14:47:21 +0300

    homework revision2

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,10 @@
-def get_mask_card_number(card_num: int) -> str:
+def get_mask_card_number(card_num: str) -> str:
     """Функция маскировки номера банковской карты"""
     total_card_num = str(card_num)
     return f"{total_card_num[:4]} {total_card_num[4:6]}** **** {total_card_num[-4:]}"
 
 
-def get_mask_account(acc_num: int) -> str:
+def get_mask_account(acc_num: str) -> str:
     """Функция маскировки номера банковского счета"""
     total_acc_num = str(acc_num)
     return f"**{total_acc_num[-4:]}"
